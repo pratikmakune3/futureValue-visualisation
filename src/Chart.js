26ff9627e75b7aed8d22ce3d4a0fb6c9cac93390
Prataik <pratikm279@gmail.com>
2017-06-16 14:47:39 +0530

    Fixing issue: Identify rects per dataset

--- src/Chart.js
+++ src/Chart.js
@@ -21,76 +21,74 @@ class Chart extends Component {
 
   renderChart(data1, data2) {
 
-      var dataArray1 = data1;
-      var dataArray2 = data2;
-      var concatDataArray = dataArray1.concat(dataArray2);
+    var dataArray1 = data1;
+    var dataArray2 = data2;
+    var concatDataArray = dataArray1.concat(dataArray2);
 
-      var concatDataArray_min = d3.min(concatDataArray);
-      var concatDataArray_max = d3.max(concatDataArray);
+    var concatDataArray_min = d3.min(concatDataArray);
+    var concatDataArray_max = d3.max(concatDataArray);
 
-      var width = 400;
+    var width = 400;
 
-      var scaleHeight = d3.scaleLinear()
-        .domain([concatDataArray_min, concatDataArray_max])
-        .range([1, 200]);
+    var scaleHeight = d3.scaleLinear()
+      .domain([concatDataArray_min, concatDataArray_max])
+      .range([1, 200]);
 
-      var scaleWidth1 = d3.scaleBand()
-        .domain(dataArray1.map(function (d, i) { return i; }))
-        .rangeRound([0, width])
-        .paddingInner(0.1)
-        .paddingOuter(0.5);
+    var scaleWidth1 = d3.scaleBand()
+      .domain(dataArray1.map(function (d, i) { return i; }))
+      .rangeRound([0, width])
+      .paddingInner(0.1)
+      .paddingOuter(0.5);
 
-      var scaleWidth2 = d3.scaleBand()
-        .domain(dataArray2.map(function (d, i) { return i; }))
-        .rangeRound([0, width])
-        .paddingInner(0.1)
-        .paddingOuter(0.5);
+    var scaleWidth2 = d3.scaleBand()
+      .domain(dataArray2.map(function (d, i) { return i; }))
+      .rangeRound([0, width])
+      .paddingInner(0.1)
+      .paddingOuter(0.5);
 
-      var yAxisScale = d3.scaleLinear()
-        .domain([concatDataArray_min, concatDataArray_max])
-        .range([200, 0]);
+    var yAxisScale = d3.scaleLinear()
+      .domain([concatDataArray_min, concatDataArray_max])
+      .range([200, 0]);
 
-      var xAxisScale = d3.scaleLinear()
-        .domain([0, dataArray1.length])
-        .range([0, width]);
+    var xAxisScale = d3.scaleLinear()
+      .domain([0, dataArray1.length])
+      .range([0, width]);
 
-      var yAxis = d3.axisLeft(yAxisScale);
-      var xAxis = d3.axisBottom(xAxisScale).ticks(dataArray1.length);
+    var yAxis = d3.axisLeft(yAxisScale);
+    var xAxis = d3.axisBottom(xAxisScale).ticks(dataArray1.length);
 
-      const svg = d3.select(this.node).select('svg');
+    const svg = d3.select(this.node).select('svg');
 
-      svg.select('.y-axis').call(yAxis);
-      svg.select('.x-axis').call(xAxis);
+    svg.select('.y-axis').call(yAxis);
+    svg.select('.x-axis').call(xAxis);
 
-      const rects1 = svg.selectAll('rect')
-        .data(dataArray1, function(d, i) { console.log(`rect1-${i}-${d}`); return `rects1-${i}`; })
+    const rects1 = svg.selectAll('rect').filter('.chart_rect1')
+      .data(dataArray1, function(d, i) { console.log(`rect1-${i}-${d}`); return `rect1-${i}`; })
 
-      rects1.exit().remove();
+    rects1.exit().remove();
 
-      const rects2 = svg.selectAll('rect')
-        .data(dataArray2, function(d, i) { console.log(`rect2-${i}-${d}`); return `rects2-${i}`; })
+    const rects2 = svg.selectAll('rect').filter('.chart_rect2')
+      .data(dataArray2, function(d, i) { console.log(`rect2-${i}-${d}`); return `rect2-${i}`; })
 
-      rects2.exit().remove();
+    rects2.exit().remove();
 
-      rects1.enter()
-        .append('rect')
-          .attr('fill', 'steelblue')
-        .merge(rects1)
-          .attr('x', function(d, i) { return scaleWidth1(i)+50; })
-          .attr('y', function(d) { return (200-scaleHeight(d)+50) })
-          .attr('height', function(d) { return scaleHeight(d); })
-          .attr('width', function () { return scaleWidth1.bandwidth()/2; });
+    rects1.enter()
+      .append('rect')
+        .attr('class', 'chart_rect1')
+      .merge(rects1)
+        .attr('x', function(d, i) { return scaleWidth1(i)+50; })
+        .attr('y', function(d) { return (200-scaleHeight(d)+50) })
+        .attr('height', function(d) { return scaleHeight(d); })
+        .attr('width', function () { return scaleWidth1.bandwidth()/2; });
 
-      rects2.enter()
-        .append('rect')
-          .attr('fill', 'midnightblue')
-        .merge(rects2)
-          .attr('x', function(d, i) { return (scaleWidth2(i)+scaleWidth1.bandwidth()/2)+50; })
-          .attr('y', function(d) { return (200-scaleHeight(d)+50) })
-          .attr('height', function(d) { return scaleHeight(d); })
-          .attr('width', function () { return scaleWidth2.bandwidth()/2; });
-
-      console.log('------------------------');
+    rects2.enter()
+      .append('rect')
+        .attr('class', 'chart_rect2')
+      .merge(rects2)
+        .attr('x', function(d, i) { return (scaleWidth2(i)+scaleWidth1.bandwidth()/2)+50; })
+        .attr('y', function(d) { return (200-scaleHeight(d)+50) })
+        .attr('height', function(d) { return scaleHeight(d); })
+        .attr('width', function () { return scaleWidth2.bandwidth()/2; });
 
   }
 
