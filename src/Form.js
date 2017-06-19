6725223d9ca08ce35453c50e07b2dca941ce50c6
Prataik <pratikm279@gmail.com>
2017-06-19 15:42:40 +0530

    Adding inputs for multiple Amount/Interest rates

--- src/Form.js
+++ src/Form.js
@@ -4,60 +4,115 @@ class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      amount: 0,
-      interest_rate: 0,
+      amount1: 0,
+      amount2: 0,
+      interest_rate1: 0,
+      interest_rate2: 0,
+      interest_rate3: 0,
+      interest_rate4: 0,
       years: 0
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleAmountChange = this.handleAmountChange.bind(this);
-    this.handleInterestChange = this.handleInterestChange.bind(this);
-    this.handleYearChange = this.handleYearChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleAmountChange(event) {
-    this.setState({amount: event.target.value});
-  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-  handleInterestChange(event) {
-    this.setState({interest_rate: event.target.value});
-  }
-
-  handleYearChange(event) {
-    this.setState({years: event.target.value});
+    this.setState({
+      [name]: value
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    var future_value_data_array = [];
-    var principle_amnt = this.state.amount;
-    var rate = this.state.interest_rate;
+
+    var future_value_data_array1 = [];
+    var future_value_data_array2 = [];
+    var future_value_data_array3 = [];
+    var future_value_data_array4 = [];
+
+    var principle_amnt1 = this.state.amount1;
+    var principle_amnt2 = this.state.amount2;
+
+    var rate1 = this.state.interest_rate1;
+    var rate2 = this.state.interest_rate2;
+    var rate3 = this.state.interest_rate3;
+    var rate4 = this.state.interest_rate4;
     var years = this.state.years;
 
     for(var i=1; i<=years; i++){
-      var pow = Math.pow((1+rate/100), i);
-      var future_value = principle_amnt * pow;
-      future_value_data_array.push(future_value);
+      var pow1 = Math.pow((1+rate1/100), i);
+      var pow2 = Math.pow((1+rate2/100), i);
+      var pow3 = Math.pow((1+rate3/100), i);
+      var pow4 = Math.pow((1+rate4/100), i);
+
+      var future_value1 = principle_amnt1 * pow1;
+      var future_value2 = principle_amnt1 * pow2;
+
+      var future_value3 = principle_amnt2 * pow3;
+      var future_value4 = principle_amnt2 * pow4;
+
+      future_value_data_array1.push(future_value1);
+      future_value_data_array2.push(future_value2);
+      future_value_data_array3.push(future_value3);
+      future_value_data_array4.push(future_value4);
+
     }
 
-    this.props.setData(future_value_data_array);
+    this.props.setData_chart1(future_value_data_array1, future_value_data_array2);
+    this.props.setData_chart2(future_value_data_array3, future_value_data_array4);
 
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+      <label>
+        Number of Years:
+        <input type="text" name='years' onChange={this.handleInputChange} />
+      </label>
+      <br /><br />
         <label>
-          Amount(in Rs.):
-          <input type="text" onChange={this.handleAmountChange} />
+          Amount-1(in Rs.):
+          <input type='text' name='amount1' onChange={this.handleInputChange} />
         </label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+
         <label>
-          Interest Rate:
-          <input type="text" onChange={this.handleInterestChange} />
+          Interest Rate 1:
+          <input type="text" name='interest_rate1' onChange={this.handleInputChange} />
         </label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+
         <label>
-          Number of Years:
-          <input type="text" onChange={this.handleYearChange} />
+          Interest Rate 2:
+          <input type="text" name='interest_rate2' onChange={this.handleInputChange} />
         </label>
+        <br />
+        <br />
+
+        <label>
+          Amount-2(in Rs.):
+          <input type='text' name='amount2' onChange={this.handleInputChange}/>
+        </label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+
+        <label>
+          Interest Rate 3:
+          <input type="text" name='interest_rate3' onChange={this.handleInputChange} />
+        </label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+
+        <label>
+          Interest Rate 4:
+          <input type="text" name='interest_rate4' onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <br />
+
         <input type="submit" value="Submit" />
       </form>
     );
